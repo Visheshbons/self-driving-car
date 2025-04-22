d45ec1ee1268de13d68bfe5648aee730c0f740f5
Vishesh Kudva <vishesh.kudva@outlook.com>
2025-04-22 21:10:18 +1200

    Added lanes

--- road.js
+++ road.js
@@ -12,6 +12,11 @@ class Road {
         this.bottom = infinity;
     };
 
+    getLaneCenter(laneIndex) {
+        const laneWidth = this.width / this.laneCount;
+        return this.left + laneWidth / 2 + Math.min(laneIndex, this.laneCount - 1) * laneWidth;
+    };
+
     draw(ctx) {
         ctx.lineWidth = 5;
         ctx.strokeStyle = 'white';
@@ -23,6 +28,11 @@ class Road {
                 i/this.laneCount
             );
 
+            if (i > 0 && i < this.laneCount) {
+                ctx.setLineDash([20,20])
+            } else {
+                ctx.setLineDash([])
+            };
             ctx.beginPath();
             ctx.moveTo(x, this.top);
             ctx.lineTo(x,this.bottom);
