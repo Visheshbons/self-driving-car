6bb1aa66acf0bebdee0d8a0d02608475a43e6dd5
Vishesh Kudva <vishesh.kudva@outlook.com>
2025-04-23 13:38:56 +1200

    Added traffic colision physics

--- main.js
+++ main.js
@@ -13,20 +13,20 @@ animate();
 
 function animate() {
     for (let i = 0; i < traffic.length; i++) {
-        traffic[i].update(road.borders);
+        traffic[i].update(road.borders, []);
     };
+    car.update(road.borders, traffic);
 
     canvas.height = window.innerHeight;
-    car.update(road.borders);
 
     ctx.save();
     ctx.translate(0, -car.y + canvas.height * 0.7);
 
     road.draw(ctx);
     for (let i = 0; i < traffic.length; i++) {
-        traffic[i].draw(ctx);
+        traffic[i].draw(ctx, 'red');
     };
-    car.draw(ctx);
+    car.draw(ctx, 'blue');
 
     ctx.restore();
     requestAnimationFrame(animate);
