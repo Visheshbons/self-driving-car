bcbb3fd9e832677e5dc8f7bab0bbfb2af2f8bb54
Vishesh Kudva <vishesh.kudva@outlook.com>
2025-05-08 14:50:15 +1200

    Update main.js

--- GameKeys/main.js
+++ GameKeys/main.js
@@ -104,9 +104,9 @@ function animate() {
         ctx.fillStyle = "black"; // Set text color to black
         ctx.font = "30px Monospace";
         ctx.fillText("Speed: " + shownSpeed + "kph", canvas.width / 2, 100); // Fixed position at the top
-        if (car.controls.nitrous) {
+        if (!car.damaged && car.controls?.nitrous) {
             ctx.fillText("NITROUS ENABLED", canvas.width / 2, 300);
-        };
+        }
 
         score = -car.y;
         requestAnimationFrame(animate);
